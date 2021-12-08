3bb270b281a4932228085b334aff8d9bc9d9508f
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-12-07 20:03:39 -0600

    feat: adds part two day two puzzle solution

---
+++ 2021/2b.py
@@ -0,0 +1,30 @@
+# Puzzle from AoC https://adventofcode.com/2021/day/1
+
+def get_command_from_file(fileobj):
+    line_str = fileobj.readline().strip()
+    if(line_str != ""):
+        command_line = line_str.split(' ')
+        return (command_line[0], int(command_line[1]))
+    else:
+        return None
+
+depth_readings_file_path = './second_input.txt'
+depth_readings = open(depth_readings_file_path, 'r')
+
+reading = get_command_from_file(depth_readings)
+depth = 0
+horizontal = 0
+aim = 0
+while reading != None:
+  if reading[0] == "up":
+    aim -= reading[1]
+  elif reading[0] == "down":
+    aim += reading[1]
+  elif reading[0] == "forward":
+    horizontal += reading[1]
+    depth += aim * reading[1]
+  reading = get_command_from_file(depth_readings)
+
+print(depth)
+print(horizontal)
+print(depth * horizontal)
