e69c99ee9ecd23e6dc8ee7310ed2607106b6382c
Scott Popplewell <scottdpopplewell@protonmail.com>
2021-12-07 19:55:10 -0600

    feat: adds second day first part. Refactors filenames for two part puzzles

---
+++ 2021/1b.py
@@ -0,0 +1,30 @@
+# Puzzle from AoC https://adventofcode.com/2021/day/1
+
+def get_num_from_file(fileobj):
+    line_str = fileobj.readline().strip()
+    if(line_str != ""):
+        return int(line_str)
+    else:
+        return None
+
+depth_readings_file_path = './first_input_part2.txt'
+depth_readings = open(depth_readings_file_path, 'r')
+
+count = 0
+
+first_reading = get_num_from_file(depth_readings)
+second_reading = get_num_from_file(depth_readings)
+third_reading = get_num_from_file(depth_readings)
+fourth_reading = get_num_from_file(depth_readings)
+
+while fourth_reading != None:
+   first_window = first_reading + second_reading + third_reading
+   second_window = second_reading + third_reading + fourth_reading
+   count += 1 if second_window - first_window > 0 else 0 
+   first_reading = second_reading
+   second_reading = third_reading
+   third_reading = fourth_reading
+   fourth_reading = get_num_from_file(depth_readings)
+
+print(count)
+
--- 2021/2.py
+++ 2021/2.py
@@ -1,30 +1,28 @@
 # Puzzle from AoC https://adventofcode.com/2021/day/1
 
-def get_num_from_file(fileobj):
+def get_command_from_file(fileobj):
     line_str = fileobj.readline().strip()
     if(line_str != ""):
-        return int(line_str)
+        command_line = line_str.split(' ')
+        return (command_line[0], int(command_line[1]))
     else:
         return None
 
 depth_readings_file_path = './second_input.txt'
 depth_readings = open(depth_readings_file_path, 'r')
 
-count = 0
+reading = get_command_from_file(depth_readings)
+depth = 0
+horizontal = 0
+while reading != None:
+  if reading[0] == "up":
+    depth -= reading[1]
+  elif reading[0] == "down":
+    depth += reading[1]
+  elif reading[0] == "forward":
+    horizontal += reading[1]
+  reading = get_command_from_file(depth_readings)
 
-first_reading = get_num_from_file(depth_readings)
-second_reading = get_num_from_file(depth_readings)
-third_reading = get_num_from_file(depth_readings)
-fourth_reading = get_num_from_file(depth_readings)
-
-while fourth_reading != None:
-   first_window = first_reading + second_reading + third_reading
-   second_window = second_reading + third_reading + fourth_reading
-   count += 1 if second_window - first_window > 0 else 0 
-   first_reading = second_reading
-   second_reading = third_reading
-   third_reading = fourth_reading
-   fourth_reading = get_num_from_file(depth_readings)
-
-print(count)
-
+print(depth)
+print(horizontal)
+print(depth * horizontal)
